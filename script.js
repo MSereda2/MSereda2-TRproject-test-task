93082f7f886608d1461c43e846372f838f9f4215
Misha <sereda2510@mail.ru>
2020-03-27 12:29:35 +0300

    Завершил тестовое задание

--- script.js
+++ script.js
@@ -24,27 +24,32 @@ const shelfUICtrl = (() => {
 
   return {
     renderShelfs: shelfs => {
-      shelfs.forEach(el => {
-        el.products.sort((a, b) => {
-          return a.productOrder - b.productOrder;
+      for (let i = 4; i >= 0; i--) {
+        const shelf = shelfs.find(val => {
+          return val.shelfOrder === i;
         });
         const markup = `
-                    <div class="rack" id="${el.shelfId}">
-                    <h1 class="rack_number">${el.shelfOrder}</h1>
-                        ${el.products.map(
-                          product =>
-                            `
-                            <div data-order="${product.productOrder}" class="product ${el.shelfId}" id="${product.productId}" draggable="true">
+                    <div class="rack" id="${shelf.shelfId}">
+                    <h1 class="rack_number">${i}</h1>
+                        ${
+                          shelf
+                            ? shelf.products.map(
+                                product =>
+                                  `
+                            <div data-order="${product.productOrder}" class="product ${shelf.shelfId}" id="${product.productId}" draggable="true">
                                 <img src="${product.productUrl}" alt="">
                             </div>
                             `
-                        )}
+                              )
+                            : ""
+                        }
+                        
                     </div>  
                 `;
         document
           .querySelector(DOMstrings.shelfsList)
           .insertAdjacentHTML("beforeend", markup);
-      });
+      }
     },
     getDOMstrings: () => {
       return DOMstrings;
@@ -72,22 +77,30 @@ const shelfUICtrl = (() => {
 })();
 
 const shelfControler = (async (shelfModal, shelfUI) => {
+
   const DOM = shelfUI.getDOMstrings();
+
+  // state of the app
   let state = {};
-  //0. state of the app
   const data = await shelfModal.getShelf();
-  console.log(data);
   state.shelfs = data;
 
+  // create new racks
+  state.shelfs.push(
+    { shelfId: 4, shelfOrder: 3, products: [] },
+    { shelfId: 5, shelfOrder: 0, products: [] }
+  );
+
+  // display object
   const str = `<p id="object"> ${JSON.stringify(data, null, 4)}</p>`;
   var markup = document.createElement("pre");
   markup.innerHTML = str;
   document.querySelector(".object_data").appendChild(markup);
 
-  //1. render shelfs with products
+  // render shelfs with products
   shelfUI.renderShelfs(state.shelfs);
 
-  //2. drag and drop products between racks
+  // drag and drop products between racks
   const products = document.querySelectorAll(DOM.products);
   const racks = document.querySelectorAll(DOM.rack_area);
 
@@ -104,20 +117,22 @@ const shelfControler = (async (shelfModal, shelfUI) => {
   racks.forEach(rack => {
     rack.addEventListener("dragover", e => {
       e.preventDefault();
+
       // get the element that is being dragged
       const draggable = document.querySelector(".draggin");
       const parentDiv = draggable.parentElement;
+
       // shelfs id
       const previousShelfId = draggable.className.split(" ")[1];
       const movedShelfId = parentDiv.id;
 
       // STEP 1: remove the product from the previous array
-      const previousShelf = state.shelfs.find(val => {
-        return parseInt(val.shelfId) === parseInt(previousShelfId);
+      const previousShelf = state.shelfs.find(value => {
+        return parseInt(value.shelfId) === parseInt(previousShelfId);
       });
       const previousShelfIdx = state.shelfs.indexOf(previousShelf);
-      const product = state.shelfs[previousShelfIdx].products.find(val => {
-        return parseInt(val.productId) === parseInt(draggable.id);
+      const product = state.shelfs[previousShelfIdx].products.find(value => {
+        return parseInt(value.productId) === parseInt(draggable.id);
       });
       const productIdx = state.shelfs[previousShelfIdx].products.indexOf(
         product
@@ -127,10 +142,10 @@ const shelfControler = (async (shelfModal, shelfUI) => {
       }
 
       // STEP 2: add the product to the new shelf's array
-      state.shelfs.forEach(val => {
-        // console.log(val.products);
-        if (parseInt(val.shelfId) === parseInt(movedShelfId)) {
-          val.products.push(product);
+      state.shelfs.forEach(value => {
+        // console.log(value.products);
+        if (parseInt(value.shelfId) === parseInt(movedShelfId)) {
+          value.products.push(product);
         }
       });
 
